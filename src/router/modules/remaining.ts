44ed744bd372f212264b046eb0b38908da2f11f0
奕初 <1244653559@qq.com>
2022-12-27 15:28:41 +0800

    fix:  修改路由

--- src/router/modules/remaining.ts
+++ src/router/modules/remaining.ts
@@ -27,5 +27,42 @@ export default [
         component: () => import("@/layout/redirect.vue")
       }
     ]
+  },
+  {
+    path: "/error",
+    component: Layout,
+    redirect: "/error/403",
+    meta: {
+      icon: "informationLine",
+      title: "异常页面",
+      showLink: false,
+      rank: 9
+    },
+    children: [
+      {
+        path: "/error/403",
+        name: "403",
+        component: () => import("@/views/error/403.vue"),
+        meta: {
+          title: "403"
+        }
+      },
+      {
+        path: "/error/404",
+        name: "404",
+        component: () => import("@/views/error/404.vue"),
+        meta: {
+          title: "404"
+        }
+      },
+      {
+        path: "/error/500",
+        name: "500",
+        component: () => import("@/views/error/500.vue"),
+        meta: {
+          title: "500"
+        }
+      }
+    ]
   }
 ] as Array<RouteConfigsTable>;
